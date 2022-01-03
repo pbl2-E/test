fe24a67a28e60b8616365941d2102c19dd5f6b6b
g187sao <87055514+g187sao@users.noreply.github.com>
2022-01-03 10:26:31 +0900

    内容登録するやつ

---
+++ add_contens.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8"/>
+<title></title>
+</head>
+<body>
+<div style="align:center;">タスクを登録いたしました。</div>
+<?php
+
+ $contents_name = $_POST['contents_name'];
+ $deadline_year = $_POST['deadline_year'];
+ $deadline_month = $_POST['deadline_month'];
+ $deadline_day = $_POST['deadline_day'];
+ $file = $_POST['task_name'].".txt";
+ $memo = $_POST['memo'];
+
+ $fw = fopen($file, "a");
+ fwrite($fw, $contents_name.",".$deadline_year.",".$deadline_month.",".$deadline_day.",".$memo."\n");
+ fclose($fw);
+ echo("\n".$contents_name."を登録しました\n");
+?>
+</body>
+</html>
