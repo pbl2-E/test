34b63321244d4c15c040759e83084e1af134a7be
g428miyo <miyoshiyuriko1@gmail.com>
2022-01-10 06:27:45 +0900

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+$_SESSION = array();
+session_destroy();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>ログアウト</title>
+</head>
+<body>
+  <h1>
+    <font size='5'>ログアウトしました</font>
+  </h1>
+  <p><a href='login.php'>ログインページに戻る</a></p>
+</body>
+</html>
