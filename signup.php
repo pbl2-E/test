fdfc0d016c4b4b46a28f8e40a2b4af12d6b556bd
g123kaji <87056115+g123kaji@users.noreply.github.com>
2022-01-10 10:39:33 +0900

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+<script type="text/javascript">
+function RegisterCheck(){
+  RegisterError();
+}
+function RegisterError(){
+  if(document.form1.address.value=="" || document.form1.password.value==""){
+  alert("メールアドレスもしくはパスワードが違います。");
+  return false;
+}
+}
+</script>
+
+<div align="center">
+<form name="form1"action="sendmail.php" method="post">
+  <br>
+  <br>
+    <br>
+  <br>
+  <input type="textarea" name="address" placeholder="メールアドレス" size=24 re\
+quired>
+  <br>
+  <br>
+  <input type="password" name="password" placeholder="パスワード" size=25 requi\
+red>
+  <br>
+  <br>
+  <input type="button" value="戻る" onClick="history.back()">
+    <input type="submit" value="登録" onClick="return RegisterCheck()">
+</form>
+</div>
+</html>
