4964ca42e4a416440a374de31bdf2182f884c5f8
g187sao <87055514+g187sao@users.noreply.github.com>
2022-01-17 10:33:51 +0900

    バージョン2.01だ！梶ちゃん

--- add_contens.php
+++ add_contens.php
@@ -5,20 +5,20 @@
 <title></title>
 </head>
 <body>
-<div style="align:center;">内容を登録いたしました。</div>
-<?php
+<div style="align:center;">タスクを登録いたしました。</div>
+<?php session_start();
 
  $contents_name = $_POST['contents_name'];
  $deadline_year = $_POST['deadline_year'];
  $deadline_month = $_POST['deadline_month'];
  $deadline_day = $_POST['deadline_day'];
- $file = $_POST['task_name'].".txt";
+ $file = $_SESSION['task_name'].".txt";
  $memo = $_POST['memo'];
 
  $fw = fopen($file, "a");
  fwrite($fw, $contents_name.",".$deadline_year.",".$deadline_month.",".$deadline_day.",".$memo."\n");
  fclose($fw);
- echo("\n".$contents_name."を登録しました\n");
+ echo("\n".$contents_name."を".$file."に登録しました\n");
 ?>
 </body>
 </html>
