4895a50e22458590d0aa359e0fff901c277c4443
g016iino <87353833+g016iino@users.noreply.github.com>
2022-01-20 12:10:22 +0900

    Create loginex.php

---
+++ loginex.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body>
+
+<?php
+
+function console_log( $data ){
+echo '<script>';
+echo 'console.log('. json_encode( $data ) .')';
+echo '</script>';
+}
+
+function LoginError(){
+   echo "メールアドレスもしくはパスワードが違います<br />";
+   echo "<a href=3_1.php>戻る</a>";
+}
+
+LoginCheck();
+function LoginCheck(){
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+
+$canma = ',';
+$ans = $email.$canma.$password;
+
+
+ //読み込みモードでファイルを開く
+$fp = fopen("passmail1.txt", "r");
+ //ファイルを１行ずつ取得する
+while ($line = fgets($fp)){
+ $trim = rtrim($line);
+ console_log($trim);
+ console_log($ans);
+ if($trim == $ans){
+   header('Location: 3.html');
+   return;
+ }
+}
+   if($ans!=$trim){
+     LoginError();
+   }
+fclose($fp);
+}
+?>
+
+</body>
+</html>
