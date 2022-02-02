e43d58a0729b254d92aa47d98af6b5433d28be67
g123kaji <87056115+g123kaji@users.noreply.github.com>
2022-02-02 10:32:35 +0900

    Update port_contents.php

--- port_contents.php
+++ port_contents.php
@@ -1,7 +1,7 @@
 <!DOCUMENT html>
 <html lang="ja">
 <head>
-<title>タスク登録</title>
+<title>内容登録</title>
 <style>
 body {
   background-image: url(mizuirohaikeiy3.jpg);
@@ -13,7 +13,7 @@ body {
 <meta charset="UTF-8">
 <script type="text/javascript">
 function HomePage(){
-         location.href='Home1.php';
+         location.href='home_test2.php';
 }
 function AddCheck(){
          task_name = document.getElementById("task_name").value;
@@ -22,7 +22,8 @@ function AddCheck(){
          deadline_day = document.getElementById("deadline_day").value;
          if(task_name=="" || deadline_year=="" || deadline_month=="" || deadline_day==""){
                      // 関数AddErrorの機能を併合
-                        document.getElementById("message").innerHTML = '<font color="red">タスク名、期限を入力してください</font>'
+                        document.getElementById("message").innerHTML = '<font color="red">内容名、期\\
+限を入力してください</font>'
                                                                      return false;
                                                                      }
                                                                      return true;
@@ -33,55 +34,68 @@ function AddCheck(){
 <br>
 <br>
 <br>
-<h1 align="center">新規タスク登録</h1>
+<h1 align="center">新規内容登録</h1>
 <br>
 <br>
-<form action="add_task2.php" method="POST">
+<form action="add_contents.php" method="POST">
+<div align="center">
 <table border="1" cellpadding=1 cellspacing=1 align="center" width="500" height="250" bgcolor=" #f2f2f2">
 <td align="center" height="40">
+<br>
+タスク名　：
+<?php
+ $task_name = $_GET['file_name'];
+ if($task_name == null){
+  $task_name = "test1";
+}
+session_start();
+$_SESSION['task_name'] = $task_name;
+echo($task_name);
+?>
+<br><br>
 <style>
 .task{
-    display: inline-block;
-    width: 10em;
-    max-width: 100%;
-    padding: 0.5em;
-    border: 1px solid #999;
-    box-sizing: border-box;
-    margin: 0.5em 0;
+   display: inline-block;
+   width: 10em;
+   max-width: 100%;
+   padding: 0.5em;
+   border: 1px solid #999;
+   box-sizing: border-box;
+   margin: 0.5em 0;
 }
 .deadline{
-    display: inline-block;
-    width: 7em;
-    max-width: 100%;
-    padding: 0.5em;
-    border: 1px solid #999;
-    box-sizing: border-box;
-    margin: 0.5em 0;
+   display: inline-block;
+   width: 7em;
+   max-width: 100%;
+   padding: 0.5em;
+   border: 1px solid #999;
+   box-sizing: border-box;
+   margin: 0.5em 0;
 }
 .deadline2{
-    display: inline-block;
-    width: 6em;
-    max-width: 100%;
-    padding: 0.5em;
-    border: 1px solid #999;
-    box-sizing: border-box;
-    margin: 0.5em 0;
+   display: inline-block;
+   width: 6em;
+   max-width: 100%;
+   padding: 0.5em;
+   border: 1px solid #999;
+   box-sizing: border-box;
+   margin: 0.5em 0;
 }
 .memo{
-    display: inline-block;
-    width: 20em;
-    max-width: 100%;
-    padding: 0.5em;
-    border: 1px solid #999;
-    box-sizing: border-box;
-    margin: 0.5em 0;
+   display: inline-block;
+   width: 20em;
+   max-width: 100%;
+   padding: 0.5em;
+   border: 1px solid #999;
+   box-sizing: border-box;
+   margin: 0.5em 0;
 }
 .back{
-    width: 100px;
-    padding: 10px;
-    box-sizing: border-box;
-    border: 1px solid #68779a;
-    background: #cbe8fa;
+   width: 100px;
+   padding: 10px;
+   box-sizing: border-box;
+   border: 1px solid #68779a;
+   background: #cbe8fa;
     cursor: pointer;
 }
 .add{
@@ -93,9 +107,8 @@ function AddCheck(){
     cursor: pointer;
 }
 </style>
-<br>
-タスク名　：<input type="text" name="task_name" class="task" size=15 ><br><br>
-期限　:<select name="deadline_year" class="deadline">
+内容　：<input type="text" name="contents_name" class="task" size=15><br><br>
+期限：<select name="deadline_year" class="deadline">
 <option>2021</option>
 <option>2022</option>
 <option>2023</option>
@@ -147,9 +160,8 @@ function AddCheck(){
 <option>30</option>
 <option>31</option>
 </select>
-<br>
-<br>
-頻度　：<textarea name="memo" rows=1 cols=25 class="memo"></textarea><br><br>
+<br><br>
+　頻度　：<textarea name="memo" class="memo" rows=1 cols=25></textarea><br><br>
 <input type="button" value="　戻る　" onclick="HomePage()" class="back">
 <input type="submit" value="　追加　" onclick="return AddCheck()" class="add"><br><br>
 </div>
