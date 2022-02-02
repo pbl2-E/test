7a07c4cb839ab26b0fb51470ac43610414e1d9a5
g123kaji <87056115+g123kaji@users.noreply.github.com>
2022-02-02 10:23:11 +0900

    Update port_contents.php
    デザイン

--- port_contents.php
+++ port_contents.php
@@ -1,26 +1,7 @@
 <!DOCUMENT html>
 <html lang="ja">
 <head>
-<title>内容登録</title>
-<meta charset="UTF-8">
-<script type="text/javascript">
-function HomePage(){
-         location.href='home_test2.php';
-}
-function AddCheck(){
-         task_name = document.getElementById("task_name").value;
-         deadline_year = document.getElementById("deadline_year").value;
-         deadline_month = document.getElementById("deadline_month").value;
-         deadline_day = document.getElementById("deadline_day").value;
-         if(task_name=="" || deadline_year=="" || deadline_month=="" || deadline_day==""){
-                     // 関数AddErrorの機能を併合
-                        document.getElementById("message").innerHTML = '<font color="red">内容名、期\\
-限を入力してください</font>'
-                                                                     return false;
-                                                                     }
-                                                                     return true;
-}
-</script>
+<title>タスク登録</title>
 <style>
 body {
   background-image: url(mizuirohaikeiy3.jpg);
@@ -29,33 +10,97 @@ body {
   background-attachment: fixed;
 }
 </style>
+<meta charset="UTF-8">
+<script type="text/javascript">
+function HomePage(){
+         location.href='Home1.php';
+}
+function AddCheck(){
+         task_name = document.getElementById("task_name").value;
+         deadline_year = document.getElementById("deadline_year").value;
+         deadline_month = document.getElementById("deadline_month").value;
+         deadline_day = document.getElementById("deadline_day").value;
+         if(task_name=="" || deadline_year=="" || deadline_month=="" || deadline_day==""){
+                     // 関数AddErrorの機能を併合
+                        document.getElementById("message").innerHTML = '<font color="red">タスク名、期限を入力してください</font>'
+                                                                     return false;
+                                                                     }
+                                                                     return true;
+}
+</script>
 </head>
 <body>
 <br>
 <br>
 <br>
+<h1 align="center">新規タスク登録</h1>
 <br>
 <br>
-<form action="add_contents.php" method="POST">
-<div align="center">
-タスク名　：
-<?php
- $task_name = $_GET['file_name'];
- if($task_name == null){
-  $task_name = "test1";
- }
- session_start();
- $_SESSION['task_name'] = $task_name;
- echo($task_name);
-?>
-<br><br>
-内容　：<input type="text" name="contents_name" size=15><br><br>
-期限：<select name="deadline_year">
+<form action="add_task2.php" method="POST">
+<table border="1" cellpadding=1 cellspacing=1 align="center" width="500" height="250" bgcolor=" #f2f2f2">
+<td align="center" height="40">
+<style>
+.task{
+    display: inline-block;
+    width: 10em;
+    max-width: 100%;
+    padding: 0.5em;
+    border: 1px solid #999;
+    box-sizing: border-box;
+    margin: 0.5em 0;
+}
+.deadline{
+    display: inline-block;
+    width: 7em;
+    max-width: 100%;
+    padding: 0.5em;
+    border: 1px solid #999;
+    box-sizing: border-box;
+    margin: 0.5em 0;
+}
+.deadline2{
+    display: inline-block;
+    width: 6em;
+    max-width: 100%;
+    padding: 0.5em;
+    border: 1px solid #999;
+    box-sizing: border-box;
+    margin: 0.5em 0;
+}
+.memo{
+    display: inline-block;
+    width: 20em;
+    max-width: 100%;
+    padding: 0.5em;
+    border: 1px solid #999;
+    box-sizing: border-box;
+    margin: 0.5em 0;
+}
+.back{
+    width: 100px;
+    padding: 10px;
+    box-sizing: border-box;
+    border: 1px solid #68779a;
+    background: #cbe8fa;
+    cursor: pointer;
+}
+.add{
+    width: 100px;
+    padding: 10px;
+    box-sizing: border-box;
+    border: 1px solid #68779a;
+    background: #cbe8fa;
+    cursor: pointer;
+}
+</style>
+<br>
+タスク名　：<input type="text" name="task_name" class="task" size=15 ><br><br>
+期限　:<select name="deadline_year" class="deadline">
 <option>2021</option>
 <option>2022</option>
 <option>2023</option>
 </select>年
-<select name="deadline_month">
+<select name="deadline_month" class="deadline2">
 <option>1</option>
 <option>2</option>
 <option>3</option>
@@ -69,7 +114,7 @@ body {
 <option>11</option>
 <option>12</option>
 </select>
-<select name="deadline_day">
+<select name="deadline_day" class="deadline2">
 <option>1</option>
 <option>2</option>
 <option>3</option>
@@ -102,11 +147,13 @@ body {
 <option>30</option>
 <option>31</option>
 </select>
-<br><br>
-　頻度　：<textarea name="memo" rows=2 cols=25></textarea><br><br>
-<input type="button" value="　ホームに戻る　" onclick="HomePage()">　　<input type="submit" value="　追\
-加　" onclick="return AddCheck()">
+<br>
+<br>
+頻度　：<textarea name="memo" rows=1 cols=25 class="memo"></textarea><br><br>
+<input type="button" value="　戻る　" onclick="HomePage()" class="back">
+<input type="submit" value="　追加　" onclick="return AddCheck()" class="add"><br><br>
 </div>
+</table>
 </form>
 </body>
 </html>
